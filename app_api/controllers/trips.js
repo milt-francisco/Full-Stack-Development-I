ce7bbf26a922d2342b4e7218dcfd5c789a229c7d
Milton Francisco <milton.francisco@snhu.com>
2025-02-09 14:00:41 -0500

    Module 6 completed baseline

--- app_api/controllers/trips.js
+++ app_api/controllers/trips.js
@@ -34,7 +34,65 @@ const tripsFindByCode = async (req, res) => {
   }
 };
 
+// POST: /trips - Adds a new Trip
+const tripsAddTrip = async (req, res) => {
+  const newTrip = new Trip({
+    code: req.body.code,
+    name: req.body.name,
+    length: req.body.length,
+    start: req.body.start,
+    resort: req.body.resort,
+    perPerson: req.body.perPerson,
+    image: req.body.image,
+    description: req.body.description,
+  });
+
+  const q = await newTrip.save();
+
+  // Show results of operation
+  // console.log(q); 
+  if (!q) {
+    return res.status(400).json(err);
+  } else {
+    return res.status(201).json(q);
+  } 
+}
+
+//PUT: /trips/:tripCode - Updates a trip
+const tripsUpdateTrip = async (req, res) => {
+  // // Debugging
+  // console.log(req.params);
+  // console.log(req.body);
+
+  const q = await Model
+    .findOneAndUpdate(
+      { 'code': req.params.tripCode },
+      {
+        code: req.body.code,
+        name: req.body.name,
+        length: req.body.length,
+        start: req.body.start,
+        resort: req.body.resort,
+        perPerson: req.body.perPerson,
+        image: req.body.image,
+        description: req.body.description,
+      }
+    )
+    .exec();
+
+    // // Show results of operation
+    // console.log(q);
+    
+    if (!q) {
+      return res.status(400).json(err);
+    } else {
+      return res.status(201).json(q);
+    }   
+}
+
 module.exports = {
   tripsList,
-  tripsFindByCode
+  tripsFindByCode,
+  tripsAddTrip,
+  tripsUpdateTrip
 };
--- app_api/routes/index.js
+++ app_api/routes/index.js
@@ -6,11 +6,13 @@ const tripsController = require("../controllers/trips");
 // route for trips endpoint
 router
   .route('/trips')
-  .get(tripsController.tripsList);
+  .get(tripsController.tripsList) // GET method routes tripList
+  .post(tripsController.tripsAddTrip); // POST method Adds a trip
 
 // GET method routes tripsFindByCode - requires 1 parameter
 router
   .route('/trips/:tripCode')
-  .get(tripsController.tripsFindByCode);
+  .get(tripsController.tripsFindByCode) // GET method route individual trip
+  .put(tripsController.tripsUpdateTrip); // PUT method updates a trip
 
 module.exports = router;
